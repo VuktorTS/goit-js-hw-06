13b845051682f939eea8f3048471bb5b10d8d696
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-02 00:59:56 +0300

    Completed task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listElement = document.querySelector("#ingredients");
+
+const arrNewLiElements = ingredients.map((ingredient) => {
+  const newLiElement = document.createElement("li");
+  newLiElement.classList.add("item");
+  newLiElement.textContent = ingredient;
+  return newLiElement;
+});
+listElement.append(...arrNewLiElements);
