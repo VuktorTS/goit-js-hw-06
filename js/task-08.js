56c0036bf21b58fbb66db5520053900a4ae59e4a
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-02 22:45:35 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(even) {
+  even.preventDefault();
+
+  const formElements = even.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  const user = {};
+
+  if (email === "" || password === "") {
+    alert("Всі поля повинні бути заповнені.");
+  } else {
+    user.password = password;
+    user.email = email;
+    console.log(user);
+    formEl.reset();
+  }
+}
