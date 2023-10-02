1d8d06fa8bf7bb736e309692879e9ac8def03f69
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-02 23:00:22 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const spanColorEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", () => {
+  const bodyColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = bodyColor;
+  spanColorEl.textContent = bodyColor;
+});
