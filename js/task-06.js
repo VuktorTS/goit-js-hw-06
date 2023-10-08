f354ac1888a50a191b6e042b03c61ff72059139d
VuktorTS <136127912+VuktorTS@users.noreply.github.com>
2023-10-08 14:04:02 +0300

    Corrections based on the mentor's comments

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,11 @@ const inputValidationElement = document.querySelector("#validation-input");
 const inputLength = inputValidationElement.getAttribute("data-length");
 
 inputValidationElement.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length >= inputLength) {
+  if (event.currentTarget.value.length === Number(inputLength)) {
     inputValidationElement.classList.add("valid");
     inputValidationElement.classList.remove("invalid");
   } else {
     inputValidationElement.classList.add("invalid");
+    inputValidationElement.classList.remove("valid");
   }
 });
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputControl = document.querySelector("#font-size-control");
 const spanTextElement = document.querySelector("#text");
 
+spanTextElement.style.fontSize = `${inputControl.value}px`;
+
 inputControl.addEventListener("input", (event) => {
   spanTextElement.style.fontSize = `${event.currentTarget.value}px`;
 });
